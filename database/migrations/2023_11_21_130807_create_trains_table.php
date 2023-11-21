67e8b1c39157bc30afec5bf0ebae23bdb3f65666
Alethepata <alessia.patane@gmail.com>
2023-11-21 14:33:55 +0100

    [edit] Schema::create

--- database/migrations/2023_11_21_130807_create_trains_table.php
+++ database/migrations/2023_11_21_130807_create_trains_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->string('agency', 100);
+            $table->string('agency', 100)->nullable();
             $table->string('departure_station',100);
             $table->string('arrival_station',100);
             $table->time('departure_time');
             $table->time('arrival_time');
             $table->string('train_code',6);
-            $table->string('number_of_carriages',50);
+            $table->string('number_of_carriages',50)->nullable();
             $table->boolean('isin_time');
             $table->boolean('isdeleted');
             $table->timestamps();
