b1636fb5f01efa12a59b10a2fef9258992fe5e55
Alethepata <alessia.patane@gmail.com>
2023-11-21 15:14:40 +0100

    [Add] PageController

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\train;
+
+class PageController extends Controller
+{
+    public function index(){
+        $train = train::all();
+        return view('home', compact('train'));
+    }
+}
